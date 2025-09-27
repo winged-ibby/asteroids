29236de06913bd267013af560c9e2282b1a88959
winged-ibby <juliorga7@gmail.com>
2025-09-27 12:29:31 -0700

    added 60 fps lock

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    fpsClock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +19,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         
+        dt = fpsClock.tick(60) / 1000
         
 
 if __name__ == "__main__":
